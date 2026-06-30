70251da8788523da183fa6e2c30e1e31c13743dc
Mr. Manohar Rajendra Sonawane <manoharofficial027@gmail.com>
2026-06-30 22:36:17 +0530

    Add SQL script for SELECT statement example
    This SQL script demonstrates the use of the SELECT statement to retrieve all records from the Employees table.

---
+++ Basic-SQL/01_SELECT.sql
@@ -0,0 +1,13 @@
+/*
+==============================================
+Topic       : SELECT Statement
+Day         : 01
+Author      : Manohar Sonawane
+Description : Display all records from a table.
+==============================================
+*/
+
+-- Display all columns from Employees table
+
+SELECT *
+FROM Employees;
