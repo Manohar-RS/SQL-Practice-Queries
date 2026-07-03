c0a096d771d4743bff8e75b010c01af0d1a4ed3e
Mr. Manohar Rajendra Sonawane <manoharofficial027@gmail.com>
2026-07-03 21:47:27 +0530

    Add SQL queries for employee data selection and count

---
+++ Basic-SQL/Day-03.sql
@@ -0,0 +1,9 @@
+-- DAY-3
+
+--Select Specific Columns into employee table
+
+select first_name, last_name, Hire_Date, Salary from employees;
+
+-- Count All Rows in selected columns
+
+select count(*) from employees;
