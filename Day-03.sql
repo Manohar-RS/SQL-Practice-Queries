46b92a47ac195a05e3b77fb195cf8c68a6152cac
Mr. Manohar Rajendra Sonawane <manoharofficial027@gmail.com>
2026-07-03 21:46:59 +0530

    Delete Day-03.sql

--- Day-03.sql
+++
@@ -1,9 +0,0 @@
--- DAY-3
-
---Select Specific Columns into employee table
-
-select first_name, last_name, Hire_Date, Salary from employees;
-
--- Count All Rows in selected columns
-
-select count(*) from employees;
