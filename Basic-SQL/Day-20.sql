676abc12360a5ccde79d339051274f396cac2f1f
Mr. Manohar Rajendra Sonawane <manoharofficial027@gmail.com>
2026-08-25 20:33:17 +0530

    Add Day 20 SQL file for join operations

---
+++ Basic-SQL/Day-20.sql
@@ -0,0 +1,17 @@
+-- Day 20
+-- Join
+
+-- Join on a non-equi conditions
+
+
+
+-- Filter a join table using WHERE
+
+
+
+-- Perform on aggregations on a joined table
+
+
+
+-- Join on a common column with different names
+
