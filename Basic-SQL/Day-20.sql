ff34a2788610736d50fc3e1e9cd802b5387f672d
Mr. Manohar Rajendra Sonawane <manoharofficial027@gmail.com>
2026-08-25 20:49:36 +0530

    Add SQL join examples to Day-20.sql
    Added SQL queries demonstrating various join operations including non-equi joins, filtering, aggregations, and joins on columns with different names.

--- Basic-SQL/Day-20.sql
+++ Basic-SQL/Day-20.sql
@@ -3,15 +3,30 @@
 
 -- Join on a non-equi conditions
 
-
+select e.employee_id , h.salary_grade 
+from employees e
+JOIN salary_grades h ON e.salary
+BETWEEN h.min_salary AND h.max_salary ;
 
 -- Filter a join table using WHERE
 
-
+select e.first_name , d.department_name 
+FROM employees e 
+JOIN departments d 
+ON e.department_id = d.department_id 
+WHERE d.location_city = 'New York' ;
 
 -- Perform on aggregations on a joined table
 
-
+select d.department_name , AVG(e.salary) 
+FROM employees e 
+JOIN departments d 
+ON e.department_id = d.department_id 
+GROUP BY d.department_name ;
 
 -- Join on a common column with different names
 
+select a.order_id , b.item_name 
+FROM table_a a
+JOIN table_b b 
+ON a.product_id = b.item_id ;
