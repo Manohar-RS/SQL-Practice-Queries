11847b91483667c69d411fc4ba920abdbeb70297
Mr. Manohar Rajendra Sonawane <manoharofficial027@gmail.com>
2026-09-03 15:22:17 +0530

    Simplify SQL queries in Day-25.sql
    Refactored SQL queries to remove unnecessary line breaks and improve readability.

--- Basic-SQL/Day-25.sql
+++ Basic-SQL/Day-25.sql
@@ -4,78 +4,34 @@
 
 -- Display each employee along with the average salary of their department.
 
-SELECT
-    name,
-    department,
-    salary,
-    AVG(salary) OVER(
-        PARTITION BY department
-    ) AS department_avg_salary
-FROM employees;
+SELECT name, department, salary, AVG(salary) OVER( PARTITION BY department) AS department_avg_salary FROM employees;
 
 
 --ROW_NUMBRS()
 -- Assign a unique row number to employees based on salary from highest to lowest.
 
-SELECT
-    name,
-    salary,
-    ROW_NUMBER() OVER(
-        ORDER BY salary DESC
-    ) AS row_number
-FROM employees;
-
+SELECT name, salary, ROW_NUMBER() OVER( ORDER BY salary DESC ) AS row_number FROM employees;
 
 -- RANK()
 --Rank employees based on salary within each department. 
 
-SELECT
-    name,
-    department,
-    salary,
-    RANK() OVER(
-        PARTITION BY department
-        ORDER BY salary DESC
-    ) AS salary_rank
-FROM employees;
+SELECT name, department, salary, RANK() OVER( PARTITION BY department ORDER BY salary DESC) AS salary_rank FROM employees;
 
 
 -- DENSE_RANK()
 -- Rank employees using DENSE_RANK() within each department.
 
-SELECT
-    name,
-    department,
-    salary,
-    DENSE_RANK() OVER(
-        PARTITION BY department
-        ORDER BY salary DESC
-    ) AS salary_rank
-FROM employees;
+SELECT name, department, salary, DENSE_RANK() OVER( PARTITION BY department ORDER BY salary DESC ) AS salary_rank FROM employees;
 
 
 -- LAG()
 --  Display each employee's salary along with the previous salary.
 
-SELECT
-    name,
-    salary,
-    LAG(salary) OVER(
-        ORDER BY salary
-    ) AS previous_salary
-FROM employees;
+SELECT name,salary, LAG(salary) OVER(ORDER BY salary) AS previous_salary FROM employees;
 
 
 -- LEAD()
 -- Display each employee's salary along with the next salary.
 
-SELECT
-    name,
-    salary,
-    LEAD(salary) OVER(
-        ORDER BY salary
-    ) AS next_salary
-FROM employees;
+SELECT name, salary, LEAD(salary) OVER( ORDER BY salary ) AS next_salary FROM employees;
 
-
--- 
